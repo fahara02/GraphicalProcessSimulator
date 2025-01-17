d3730a7607ad7d90ff6d71a63432305bfb9c4c07
fahara02 <idea3d.faruk@gmail.com>
2025-01-18 03:33:53 +0600

    Som enhacement in number selector

--- src/GPSU/RotaryEncoder/RotaryNumberSelector.hpp
+++ src/GPSU/RotaryEncoder/RotaryNumberSelector.hpp
@@ -1,64 +1,112 @@
 #ifndef ROTARY_NUMBER_SELECTOR_HPP
 #define ROTARY_NUMBER_SELECTOR_HPP
-#include "RotaryEncoder.hpp"
-#include <memory>
-namespace COMPONENT {
 
-class EncoderNumberSelector {
-private:
+#include "RotaryEncoder.hpp"
+#include "esp_log.h"
+#include <cmath>
+#include <functional>
+#include <memory>
+
+namespace COMPONENT {
+#define TAG_SELECTOR "NumberSelector"
+
+struct RangeConfig {
   float minValue = 0;
   float maxValue = 100;
   float step = 2;
-  float coeficient = 1;
+  bool cycleValues = false;
+  unsigned int decimals = 0;
+};
+
+class EncoderNumberSelector {
+private:
+  RangeConfig rangeConfig;
+  float coefficient = 1;
   std::unique_ptr<Encoder> encoder;
+  std::function<int(long)> customAccelerationMapping;
+  constexpr static int ACCELERATION_NONE = 0;
+  constexpr static int ACCELERATION_LOW = 20;
+  constexpr static int ACCELERATION_MEDIUM = 100;
+  constexpr static int ACCELERATION_HIGH = 300;
+  constexpr static int ACCELERATION_MAX = 500;
 
 public:
   EncoderNumberSelector(uint8_t steps, gpio_num_t aPin, gpio_num_t bPin,
                         gpio_num_t buttonPin = GPIO_NUM_NC,
                         bool encoderPinPulledDown = true,
-                        bool buttonPulledDown = false)
+                        bool buttonPulledDown = false,
+                        std::function<int(long)> customMapping = nullptr)
       : encoder(std::make_unique<Encoder>(steps, aPin, bPin, buttonPin,
                                           encoderPinPulledDown,
-                                          buttonPulledDown)) {}
+                                          buttonPulledDown)),
+        customAccelerationMapping(customMapping) {}
 
-  void setRange(float minValue, float maxValue, float step, bool cycleValues,
-                unsigned int decimals = 0) {
-    this->minValue = minValue;
-    this->maxValue = maxValue;
-    this->coeficient = pow(10.0, decimals);
-    if (maxValue < minValue)
-      coeficient *= -1.0;
-    this->step = step * this->coeficient;
+  // Struct-based setRange
+  void setRange(const RangeConfig &config) {
+    if (config.step <= 0) {
+      ESP_LOGE(TAG_SELECTOR, "Step value must be greater than zero.");
+      return;
+    }
+    if (config.maxValue < config.minValue) {
+      ESP_LOGE(TAG_SELECTOR, "Max value cannot be less than min value.");
+      return;
+    }
 
-    long minEncoderValue =
-        (long)((this->coeficient * this->minValue) / this->step);
-    long maxEncoderValue =
-        (long)((this->coeficient * this->maxValue) / this->step);
+    this->rangeConfig = config;
+    this->coefficient = std::pow(10.0, config.decimals);
+    float effectiveStep = config.step * this->coefficient;
+
+    long minEncoderValue = static_cast<long>(
+        (this->coefficient * config.minValue) / effectiveStep);
+    long maxEncoderValue = static_cast<long>(
+        (this->coefficient * config.maxValue) / effectiveStep);
     long range = maxEncoderValue - minEncoderValue;
 
-    encoder->setBoundaries(minEncoderValue, maxEncoderValue, cycleValues);
+    encoder->setBoundaries(minEncoderValue, maxEncoderValue,
+                           config.cycleValues);
 
-    if (range < 20)
-      encoder->setAcceleration(0);
-    else if (range < 60)
-      encoder->setAcceleration(20);
-    else if (range < 200)
-      encoder->setAcceleration(100);
-    else if (range < 1000)
-      encoder->setAcceleration(300);
-    else
-      encoder->setAcceleration(500);
+    // Apply acceleration logic
+    applyAcceleration(range);
   }
+
+  // Parameter-based overload of setRange
+  void setRange(float minValue, float maxValue, float step, bool cycleValues,
+                unsigned int decimals = 0) {
+    RangeConfig config = {minValue, maxValue, step, cycleValues, decimals};
+    setRange(config); // Delegate to the struct-based version
+  }
+
   void setValue(float value) {
-    long encoderValue = (long)((coeficient * value) / step);
-    encoder->setEncoderValue(encoderValue);
+    if (rangeConfig.step <= 0) {
+      ESP_LOGE(TAG_SELECTOR, "Step value must be greater than zero.");
+    } else {
+      long encoderValue = static_cast<long>((coefficient * value) /
+                                            (rangeConfig.step * coefficient));
+      encoder->setEncoderValue(encoderValue);
+    }
   }
 
   float getValue() {
-    float encoderValue = 1.0 * encoder->readEncoder();
-    float value = encoderValue * step / coeficient;
+    long encoderValue = encoder->readEncoder();
+    return static_cast<float>(encoderValue) * rangeConfig.step / coefficient;
+  }
 
-    return value;
+private:
+  void applyAcceleration(long range) {
+    if (customAccelerationMapping) {
+      encoder->setAcceleration(customAccelerationMapping(range));
+    } else {
+      if (range < 20)
+        encoder->setAcceleration(ACCELERATION_NONE);
+      else if (range < 60)
+        encoder->setAcceleration(ACCELERATION_LOW);
+      else if (range < 200)
+        encoder->setAcceleration(ACCELERATION_MEDIUM);
+      else if (range < 1000)
+        encoder->setAcceleration(ACCELERATION_HIGH);
+      else
+        encoder->setAcceleration(ACCELERATION_MAX);
+    }
   }
 };
 
