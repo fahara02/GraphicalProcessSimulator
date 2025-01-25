bc3ff664e56c9ce0f3a4314cb05e0ce8c9435bbd
fahara02 <idea3d.faruk@gmail.com>
2025-01-25 22:08:58 +0600

    chaning code architecture

---
+++ src/GPSU/Components/MCP23017/MCP_GPIO_banks.hpp
@@ -0,0 +1,219 @@
+#ifndef MCP_GPIO_BANKS_HPP
+#define MCP_GPIO_BANKS_HPP
+#include "MCP_Primitives.hpp"
+#include "MCP_Registers.hpp"
+namespace MCP {
+class GPIO_BANKS {
+private:
+  std::array<Pin, PIN_PER_BANK> Pins;
+  std::array<bool, PIN_PER_BANK> pinInterruptState;
+
+public:
+  constexpr GPIO_BANKS(PORT port, bool enableInterrupt = false)
+      : Pins(createPins(port)), pinInterruptState{false},
+        interruptEnabled(enableInterrupt),
+        generalMask(static_cast<uint8_t>(MASK::ALL)), interruptMask(0x00),
+        port_name(port), ports(0), ddr(0), pull_ups(0),
+        intr_type(INTR_TYPE::NONE), intr_out_type(INTR_OUTPUT_TYPE::NA) {
+    assert(isValidPort(port) && "Invalid PORT provided!");
+  }
+  template <typename... PinType>
+  constexpr GPIO_BANKS(PORT port, PinType... pins)
+      : Pins(fillPinsArray(pins...)), pinInterruptState{false},
+        interruptEnabled(false), generalMask(calculateMask(pins...)),
+        interruptMask(0x00), port_name(port), ports(0), ddr(0), pull_ups(0),
+        intr_type(INTR_TYPE::NONE), intr_out_type(INTR_OUTPUT_TYPE::NA) {
+    // Validate port
+    assert(isValidPort(port) && "Invalid PORT provided!");
+
+    // Validate that all pins belong to the same port
+    assert(validatePins(port, pins...) &&
+           "All pins must belong to the same port!");
+  }
+
+  void init() {
+
+    updatePins();
+    if (interruptEnabled) {
+      updatePinInterruptState();
+    }
+  }
+  // Pin masks
+  void setGeneralMask(MASK mask) {
+    generalMask = static_cast<uint8_t>(mask);
+    updatePins();
+    if (interruptEnabled) {
+      updatePinInterruptState();
+    }
+  }
+
+  void setGeneralMask(uint8_t mask) {
+    // Ensure the mask is valid for the current configuration of PIN_PER_BANK
+    static_assert(PIN_PER_BANK <= 8, "Only 8-bit masks are supported!");
+
+    // Assert that the mask fits within the valid range
+    assert((mask & ~((1 << PIN_PER_BANK) - 1)) == 0 &&
+           "Invalid mask: must be within the valid pin range!");
+
+    generalMask = mask;
+    updatePins();
+    if (interruptEnabled) {
+      updatePinInterruptState();
+    }
+  }
+
+  uint8_t getGeneralMask() const { return generalMask; }
+
+  uint8_t getInterruptMask() const { return interruptMask; }
+
+  // Get the pin state by index
+  PIN_STATE getPinState(uint8_t index) const {
+    if (index >= PIN_PER_BANK) {
+      return PIN_STATE::UNDEFINED; // Invalid index
+    }
+    return Pins[index].getState();
+  }
+  uint8_t getPinStates() const {
+    uint8_t result = 0;
+    for (uint8_t i = 0; i < PIN_PER_BANK; ++i) {
+      if (generalMask & (1 << i)) { // Check if pin is selected by the mask
+        result |= (Pins[i].getState() == PIN_STATE::ON ? (1 << i) : 0);
+      }
+    }
+    return result;
+  }
+  // Set the pin state by index
+  void setPinState(uint8_t index, PIN_STATE state) {
+    // Validate PIN_PER_BANK at compile-time
+    static_assert(PIN_PER_BANK <= 8, "Invalid PIN_PER_BANK configuration!");
+
+    // Ensure index is within valid range at runtime
+    assert(index < PIN_PER_BANK && "Invalid pin index!");
+
+    // Ensure the pin is enabled by the general mask
+    if (!(generalMask & (1 << index))) {
+      assert(false && "Pin is not enabled by the general mask!");
+      return;
+    }
+
+    Pins[index].setState(state); // Set the pin state
+  }
+
+  void setPinState(PIN_STATE state) {
+    for (uint8_t i = 0; i < PIN_PER_BANK; ++i) {
+      if (generalMask & (1 << i)) {
+        Pins[i].setState(state);
+      }
+    }
+  }
+
+  void setupInterrupt(uint8_t pinMask = 0xFF,
+                      INTR_TYPE intrType = INTR_TYPE::NONE,
+                      INTR_OUTPUT_TYPE intrOutType = INTR_OUTPUT_TYPE::NA) {
+    uint8_t maskToApply = pinMask & generalMask;
+    for (uint8_t i = 0; i < PIN_PER_BANK; ++i) {
+      if (maskToApply & (1 << i)) {
+        intr_type = intrType;
+        intr_out_type = intrOutType;
+
+        pinInterruptState[i] = true; // Update interrupt state
+      } else {
+        pinInterruptState[i] = false;
+      }
+    }
+    setInterruptMask(maskToApply);
+  }
+
+  // Set pull-up resistor for a pin by index
+  void setPullup(uint8_t index, bool enable) {}
+
+  // Set pin direction (INPUT or OUTPUT) by index
+  void setPinDirection(uint8_t index, GPIO_MODE mode) {}
+
+  bool isInterruptEnabled() const { return interruptEnabled; }
+
+private:
+  bool interruptEnabled;
+  uint8_t generalMask;
+  uint8_t interruptMask;
+  PORT port_name;
+  uint8_t ports;
+  uint8_t ddr;
+  uint8_t pull_ups;
+  INTR_TYPE intr_type;
+  INTR_OUTPUT_TYPE intr_out_type;
+
+  void setInterruptMask(uint8_t pinMask) {
+    interruptMask = pinMask & 0xFF;
+    if (interruptEnabled) {
+      updatePinInterruptState();
+    }
+  }
+  void updatePins() {
+    for (uint8_t i = 0; i < PIN_PER_BANK; ++i) {
+      bool isGeneralMaskSet = BitUtil::isBitSet(generalMask, i);
+
+      if (!isGeneralMaskSet) {
+
+        Pins[i].setState(PIN_STATE::UNDEFINED);
+      }
+    }
+  }
+
+  void updatePinInterruptState() {
+    for (uint8_t i = 0; i < PIN_PER_BANK; ++i) {
+      bool isGeneralMaskSet = BitUtil::isBitSet(generalMask, i);
+      bool isInterruptMaskSet = BitUtil::isBitSet(interruptMask, i);
+      pinInterruptState[i] = isGeneralMaskSet && isInterruptMaskSet;
+    }
+  }
+
+  static constexpr bool isValidPort(PORT port) {
+    return port == PORT::GPIOA || port == PORT::GPIOB;
+  }
+
+  static constexpr std::array<Pin, PIN_PER_BANK> createPins(PORT port) {
+    std::array<Pin, PIN_PER_BANK> pins{};
+    for (size_t i = 0; i < PIN_PER_BANK; ++i) {
+      pins[i] =
+          Pin(static_cast<PIN>(static_cast<uint8_t>(port) * PIN_PER_BANK + i));
+    }
+    return pins;
+  }
+
+  // Helper to calculate the general mask
+  template <typename... PinType>
+  static constexpr uint8_t calculateMask(PinType... pins) {
+    return (0 | ... | pins.getMask());
+  }
+
+  // Helper to validate pins belong to the same port
+  template <typename... PinType>
+  static constexpr bool validatePins(PORT port, PinType... pins) {
+    return ((pins.getPort() == port) && ...);
+  }
+  template <typename... PinType>
+  static constexpr std::array<Pin, PIN_PER_BANK>
+  fillPinsArray(PinType... pins) {
+    std::array<Pin, PIN_PER_BANK> pinArray = {};
+    size_t i = 0;
+
+    // Assign provided pins
+    ((pinArray[i++] = pins), ...);
+
+    // Fill remaining Pins with default
+    while (i < PIN_PER_BANK) {
+      pinArray[i++] = Pin();
+    }
+
+    return pinArray;
+  }
+};
+// constexpr GPIO_BANKS<MCP23017Pin> gpioBankA(PORT::GPIOA);
+// // Define GPIO Bank instances
+constexpr GPIO_BANKS BANK_A(PORT::GPIOA);
+
+// Using the variadic constructor
+constexpr GPIO_BANKS bankA_custom(PORT::GPIOA, GPA0, GPA1, GPA2);
+} // namespace MCP
+#endif
